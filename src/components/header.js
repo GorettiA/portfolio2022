f9969c3b11763c1a764b711a9a1502f27c0078ae
Goretti Alani <gorettialani@gmail.com>
2022-02-10 13:59:04 +1300

    update readme file

---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Header() {
+  return <div>
+    <h1>Header</h1>
+  </div>;
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Navbar() {
+  return <div>
+    <h1>Navbar</h1>
+  </div>;
+}
