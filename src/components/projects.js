bd0b9e7a08cb011dd96f94dfaa8a404b57887450
Goretti Alani <gorettialani@gmail.com>
2022-02-14 19:08:59 +1300

    add first project link

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,6 +2,12 @@ import React from 'react';
 import '../componentsCSS/projects.css';
 import { FaFileAlt } from 'react-icons/fa';
 
+function projectLink (){
+    return(
+        <div className="project-link">
+            <a href="https://donalds-dumb-doings.herokuapp.com"></a>
+            </div>
+    )}
 
 export default function Projects() {
   return <div className='main-body'>
@@ -13,10 +19,13 @@ export default function Projects() {
     </div>
 
   <div className='body-right'>
-  <h2>My Work</h2>
+  <h2>Stuff I've worked on so far...</h2>
 
   <div className='project'>
-    <p>project 1</p>
+    <h4>Having fun Consuming External API's</h4>
+    <a href="https://donalds-dumb-doings.herokuapp.com" target="_blank" rel="noreferrer">
+      <button className="project-link">Donald's Dumb Doings</button>
+    </a>
   </div>
   </div>
 
