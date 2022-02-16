d2ba74f5fe6da08a354ad9d5bcd4460124f30f41
Goretti Alani <gorettialani@gmail.com>
2022-02-16 15:09:53 +1300

    add new styling for navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,48 @@
 import React from 'react';
 import '../componentsCSS/navbar.css';
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 export default function Navbar() {
-  return <div className='navbar'>
-    {/* <button className='nav-link'>About Me</button>
-    <button className='nav-link'>Get in Touch</button>
-    <button className='nav-link'>Get my CV</button> */}
-  <FaGithub className='icon'/>
-    <FaLinkedin className='icon' /> 
-    <FaEnvelope className='icon'/> 
-  </div>;
+	return (
+		<nav className="navbar">
+			<ul>
+				<li>
+					<h1>
+						<a href="index.html">Goretti Alani</a>
+					</h1>
+				</li>
+				<li>
+					<a href="#projects">Projects</a>
+				</li>
+				<li>
+					<a href="about.html">About</a>
+				</li>
+				<li>
+					<a href="#contact">
+						<span className="fa-solid fa-envelope" aria-hidden="true"></span>
+						<span className="sr-only">Contact</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://www.linkedin.com/in/jen4web/" target="_blank" rel="noreferrer">
+						<span class="fa-brands fa-linkedin" aria-hidden="true"></span>
+						<span className="sr-only">LinkedIn</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://www.github.com/jen4web" target="_blank" rel="noreferrer">
+						<span class="fa-brands fa-github" aria-hidden="true"></span>
+						<span className="sr-only">Github</span>
+					</a>
+				</li>
+				<li>
+					<a
+						className="button"
+						href="https://www.dropbox.com/s/3z2hm5gu3v2lrfy/JenKramerCV.pdf?dl=0"
+					>
+						Resume
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,57 +1,10 @@
 import React from 'react';
 import '../componentsCSS/projects.css';
-import { FaFileAlt } from 'react-icons/fa';
 
-function projectLink (){
-    return(
-        <div className="project-link">
-            <a href="https://donalds-dumb-doings.herokuapp.com"></a>
-            </div>
-    )}
 
 export default function Projects() {
   return <div className='main-body'>
-    <div className='body-left'>
-    <h2>Talofa Lava!</h2>
-    <p>I'm a web developer based in Auckland, New Zealand</p>
-    <p>I love to create web apps using ReactJS</p>
-    <p><FaFileAlt className='icon'/>Check out my resume</p>
-    </div>
-
-  <div className='body-right'>
-  <h2>Stuff I've worked on so far...</h2>
-
-  <div className='project'>
-    <h4>Having fun Consuming External API's</h4>
-    <p>Work in pairs to select an external API and consume it in ReactJS </p>
-    <a href="https://donalds-dumb-doings.herokuapp.com" target="_blank" rel="noreferrer">
-      <button className="project-link">Donald's Dumb Doings</button>
-    </a>
-  </div>
-
-  <div className='project'>
-    <h4>Final full stack project from Mission Ready HQ</h4>
-    <p>A learning management system for teachers to deliver digital
-      technologies curriculum - specifically to learn programming, in
-      an easy way.
-    </p>
-    <a href="https://donalds-dumb-doings.herokuapp.com" target="_blank" rel="noreferrer">
-      <button className="project-link">Mission X</button>
-    </a>
-  </div>
-
-  <div className='project'>
-    <h4>An ongoing project created in collaboration with my
-      classmates at Dev Academy
-    </h4>
-    <p>A full stack app that allows trainees/apprentices to find work within their
-      community for koha.
-    </p>
-    <a href="https://trainee-jobs-dev.herokuapp.com/" target="_blank" rel="noreferrer">
-      <button className="project-link">Trainee Jobs</button>
-    </a>
-  </div>
-  </div>
+   
 
   </div>
 }
